e410773ffc99c77f4cb2c8fcb7ff19d67d50bba8
dlloigit36 <87413510+dlloigit36@users.noreply.github.com>
2021-07-14 17:10:09 +0800

    Update iisinstall.ps1

--- iisinstall.ps1
+++ iisinstall.ps1
@@ -2,3 +2,4 @@ mkdir c:\buildArtifacts
 echo Azure-Image-Builder-Was-Here  > c:\buildArtifacts\azureImageBuilder.txt
 ping localhost > c:\buildArtifacts\pinglocalhost.txt
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
+ping localhost > c:\buildArtifacts\pinglocalhost2.txt
