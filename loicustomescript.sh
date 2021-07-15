26d56de5df571449059253cb4ee2ce7f7f3089e7
dlloigit36 <87413510+dlloigit36@users.noreply.github.com>
2021-07-15 17:21:13 +0800

    Update loicustomescript.sh

--- loicustomescript.sh
+++ loicustomescript.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -e
 
-mkdir /tmp/staging
-touch /tmp/staging/creativityAtItsBest.md
+# Add preview banner to MOTD
+cat >> /etc/motd << EOF
+*******************************************************
+**            This VM was built from the: 1234       **
+**      !! AZURE VM IMAGE BUILDER Custom Image !!    **
+**         You have just been Customized :-)         **
+*******************************************************
+EOF
 
 mkdir /loiscriptfolder
 df -h > /loiscriptfolder/dfoutput.txt
 sudo apt-get -y update > /loiscriptfolder/updatestatus.txt
 sudo apt-get -y install nginx > /loiscriptfolder/nginx.txt
-df -h > /loiscriptfolder/dfoutput2.txt"
+df -h > /loiscriptfolder/dfoutput2.txt
